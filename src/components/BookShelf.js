5382ac2fb7ae24f67615269734490639a5aeed60
Sylvia Maguiña <smag23@gmail.com>
2018-03-15 15:09:26 -0700

    Create BookShelf component
    This maps through the ListBooks component to list the books for each
    shelf. Book component is called upon.

---
+++ src/components/BookShelf.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Book from './Book';
+
+function BookShelf (props) {
+  const { books, sectionName, onShelfChange } = props
+
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{sectionName}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {books.map( book => (
+            <li key={book.id}>
+              <Book book={book} onShelfChange={onShelfChange}/>
+            </li>
+          ))}
+        </ol>
+      </div>
+    </div>
+  )
+}
+
+BookShelf.PropTypes = {
+  books: PropTypes.array.isRequired,
+  sectionName: PropTypes.string.isRequired,
+  onShelfChange: PropTypes.func.isRequired
+}
+
+export default BookShelf;
